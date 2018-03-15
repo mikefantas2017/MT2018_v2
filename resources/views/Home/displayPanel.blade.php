1dccc270e67cc7fdd8c533b5bd779d1005477414
mikefantas2017 <mikefantas2@gmail.com>
2018-03-15 02:59:14 -0600

    Omisión de gif.

--- resources/views/Home/displayPanel.blade.php
+++ resources/views/Home/displayPanel.blade.php
@@ -1,5 +1,5 @@
 {{-- Panel de visualización (con un modelo cargado)--}}
-<div id="panelView" class="panel-body" style="background: #868383; color: white; height: 100%; padding: 0px">
+<div id="panelView" class="panel-body" style="background: white; color: white; height: 100%; padding: 0px">
     <canvas id="gl" style="background: white; width: 100%; height: 100%; border: 1px solid #D8D8D8">
         "El navegador no soporta el elemento canvas...."
     </canvas>
